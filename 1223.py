d488687e992851ef012d05024140de9045311607
Altaf476 <144001371+Altaf476@users.noreply.github.com>
2023-09-08 08:58:01 +0200

    Add files via upload

---
+++ 1223.py
@@ -0,0 +1,7 @@
+def tokenize (text):
+    x= text.split()
+    print(x)
+document = ['"They had 16 rolls of duct tape, 2 bags of clothes pins,',
+'130 hampsters from the cancer labs down the hall, and','at least 500 pounds of grape jello and unknown amounts of chopped liver"',]
+tokenize(document)
+
